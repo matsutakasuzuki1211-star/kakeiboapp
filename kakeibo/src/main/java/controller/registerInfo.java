ac7df1a2c77a37c737b77f3e8537193b733b0e2e
matsutakasuzuki1211-star <matsutaka.suzuki1211@gmail.com>
2025-09-02 10:59:53 +0900

    登録画面

--- kakeibo/src/main/java/controller/registerInfo.java
+++ kakeibo/src/main/java/controller/registerInfo.java
@@ -1,5 +1,27 @@
 package controller;
 
-public class registerInfo {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+@WebServlet("/register")
+public class registerInfo extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+
+        int balance = Integer.parseInt(request.getParameter("balance"));
+        int payment = Integer.parseInt(request.getParameter("payment"));
+        
+
+        HttpSession session = request.getSession();
+        session.setAttribute("balance", balance);
+        session.setAttribute("payment", payment);
+
+        request.getRequestDispatcher("/dao.java").forward(request, response);
+    }
 }
