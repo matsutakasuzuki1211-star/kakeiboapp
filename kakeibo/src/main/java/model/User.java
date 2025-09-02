a80fa23a1815e3a71fb01af5373e8537af361c7b
matsutakasuzuki1211-star <matsutaka.suzuki1211@gmail.com>
2025-09-02 10:30:25 +0900

    javabeans作成

---
+++ kakeibo/src/main/java/model/User.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.io.*;
+
+public class User implements Serializable{
+    
+    private String username;
+    private String password;
+
+    public User(){
+        this.username = null;
+        this.password = null;
+    }
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setName(String username){
+        this.username = username;
+    }
+
+    public String getName(){
+        return this.username;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+}
